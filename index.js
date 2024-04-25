e3f15c8ae4886390f16b3b41b8ba804eb35e8448
Aravoind <161047197+Aravoind@users.noreply.github.com>
2024-04-25 18:28:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+var name=document.querySelectorAll(".drum").length
+for ( var i=0; i<name ; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click" ,function()  {
+        var head=this.innerHTML;
+        makeSound(head)
+        soundAnime(head)
+     });
+
+}
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    soundAnime(event.currentani)
+
+});
+
+function makeSound(key) {
+
+    switch(key) {
+        case "w":
+            var audio1= new Audio("sounds/crash.mp3");
+            audio1.play();
+        break;
+        case "a":
+            var audio2= new Audio("sounds/kick-bass.mp3");
+            audio2.play();
+        break;
+
+        case "s":
+            var audio3= new Audio("sounds/snare.mp3");
+            audio3.play();
+        break;
+
+        case "d":
+            var audio3= new Audio("sounds/tom-1.mp3");
+            audio3.play();
+        break;
+
+        case "j":
+            var audio4= new Audio("sounds/tom-2.mp3");
+            audio4.play();
+        break;
+
+        case "k":
+            var audio5= new Audio("sounds/tom-3.mp3");
+            audio5.play();
+        break;
+
+        case "l":
+            var audio6= new Audio("sounds/tom-4.mp3");
+            audio6.play();
+        break;
+
+        default:
+    }
+}
+function soundAnime(currentani){
+    var activebutton=document.querySelector("." + currentani);
+    activebutton.classList.add("pressed");
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    },100)
+
+}
